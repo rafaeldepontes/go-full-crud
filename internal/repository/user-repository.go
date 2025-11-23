ba24f2c10f320b75d70a42f49dd99ede21997bae
rafaeldepontes <rafael.cr.carneiro@gmail.com>
2025-11-23 01:10:59 -0300

    feat: adding User Repository

---
+++ internal/repository/user-repository.go
@@ -0,0 +1,88 @@
+package repository
+
+import (
+	"database/sql"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+)
+
+type User struct {
+	Id        *int       `json:"id"`
+	Username  *string    `json:"username"`
+	Password  *string    `json:"password"`
+	Email     *string    `json:"email,omitempty"`
+	Birthdate *string    `json:"birthdate,omitempty"`
+	CreatedAt *time.Time `json:"created_at"`
+	UpdatedAt *time.Time `json:"updated_at,omitempty"`
+}
+
+type UserRepository struct {
+	db *sql.DB
+}
+
+func NewUserRepository(db *sql.DB) *UserRepository {
+	return &UserRepository{
+		db: db,
+	}
+}
+
+type UserDatabase interface {
+	CreateUser(user User) error
+	FindUserById(id *int) (*User, error)
+	FindUserByUsername(username *string) (*User, error)
+}
+
+func (ur UserRepository) FindUserByUsername(username *string) (*User, error) {
+	user := User{}
+
+	query :=
+		`
+		SELECT id, username, password, email, birthdate, created_at, updated_at
+		FROM users
+		WHERE username = $1
+	`
+
+	err := ur.db.QueryRow(query, username).Scan(&user.Id, &user.Username, &user.Password, &user.Email, &user.Birthdate, &user.CreatedAt, &user.UpdatedAt)
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func (ur UserRepository) FindUserById(id *int) (*User, error) {
+	user := User{}
+
+	query :=
+		`
+		SELECT id, username, password, email, birthdate, created_at, updated_at
+		FROM users
+		WHERE id = $1
+	`
+
+	err := ur.db.QueryRow(query, id).Scan(&user.Id, &user.Username, &user.Password, &user.Email, &user.Birthdate, &user.CreatedAt, &user.UpdatedAt)
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func (ur UserRepository) CreateUser(user *User) error {
+	log.Info("Adding a new user")
+
+	query :=
+		`
+	INSERT INTO users (username, password, created_at)
+	VALUES ($1, $2, $3)
+	RETURNING id, created_at
+	`
+
+	err := ur.db.QueryRow(query, user.Username, user.Password, user.CreatedAt).Scan(&user.Id, &user.CreatedAt)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
