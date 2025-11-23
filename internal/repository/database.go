a404ebd3b581c7891b42cb90c338d42dbbbc326e
rafaeldepontes <rafael.cr.carneiro@gmail.com>
2025-11-23 01:10:47 -0300

    feat: adding the database.go package

---
+++ internal/repository/database.go
@@ -0,0 +1,26 @@
+package repository
+
+import (
+	"database/sql"
+	"os"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+	log "github.com/sirupsen/logrus"
+)
+
+func Open() (*sql.DB, error) {
+	conn, err := sql.Open("pgx", os.Getenv("DATABASE_URL"))
+
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+
+	if err = conn.Ping(); err != nil {
+		log.Error(err)
+		return nil, err
+	}
+
+	println("Database successfully connected...")
+	return conn, nil
+}
